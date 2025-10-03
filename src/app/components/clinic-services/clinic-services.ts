a90ecaab817c28b9421dd59e04093ba6bb08e14b
saliwanm <saliwanm@gmail.com>
2025-10-03 01:16:08 +0100

    upgrade 1

--- src/app/components/clinic-services/clinic-services.ts
+++ src/app/components/clinic-services/clinic-services.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-clinic-services',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './clinic-services.html',
   styleUrl: './clinic-services.scss'
 })
--- src/app/components/consultations/consultations.ts
+++ src/app/components/consultations/consultations.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-consultations',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './consultations.html',
   styleUrl: './consultations.scss'
 })
